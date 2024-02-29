bc76b49978ceb3bb1a87c77f7ff934063053d06a
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-02-29 20:01:59 +0530

    Create `blinkLED.ino`

---
+++ blinkLED.ino
@@ -0,0 +1,18 @@
+// LED's
+// Positive Terminal -> pin 13
+// Negative Terminal -> GND (Ground)
+
+int LED = 13;
+
+void setup()
+{
+	pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(LED, HIGH);
+	delay(1000);
+	digitalWrite(LED, LOW);
+	delay(1000);
+}
