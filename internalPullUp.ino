73e9f5adc0278ad2f39757785edd783bec865008
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-03-06 21:40:53 +0530

    Create `internalPullUp.ino`

---
+++ internalPullUp.ino
@@ -0,0 +1,29 @@
+// Pull Up Resistor - 
+// 1 / ON - LOW
+// 0 / OFF - HIGH
+
+/*
+CIRCUIT - 
+
+GND -> Push Button (path 0)
+PIN 11 -> Push Button (path 1)
+
+PIN 10 -> LED (+ve terminal)
+GND -> LED(-ve terminal)
+*/
+
+int LED = 10, RECEIVER = 11;
+
+void setup()
+{
+	pinMode(LED, OUTPUT);
+	pinMode(RECEIVER, INPUT_PULLUP);
+}
+
+void loop()
+{
+	if(digitalRead(RECEIVER) == LOW)
+		digitalWrite(LED, HIGH);
+	else
+		digitalWrite(LED, LOW);
+}
